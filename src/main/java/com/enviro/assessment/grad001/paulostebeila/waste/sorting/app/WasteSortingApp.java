6b0f9ee802579d8dc468efe67ae78c4460765a10
Paul Tebeila <paultebeila@gmail.com>
2025-01-14 19:19:39 +0200

    Project created

---
+++ src/main/java/com/enviro/assessment/grad001/paulostebeila/waste/sorting/app/WasteSortingApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.enviro.assessment.grad001.paulostebeila.waste.sorting.app;
+
+/**
+ *
+ * @author pault
+ */
+public class WasteSortingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
